1bbd7639ed11fb94c669923c641f2a7bddbb92ce
serori <seroriKETC@seroriKETC-PC>
2014-12-07 22:50:45 +0900

    無駄な処理を削除

--- app/src/main/java/com/serori/numeri/main/MainActivity.java
+++ app/src/main/java/com/serori/numeri/main/MainActivity.java
@@ -16,14 +16,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String s;
-        Button btn = (Button) findViewById(R.id.toastbutton);
-        btn.setOnClickListener(l->{show("onClick");});
+    }
 
-    }
-    private void show(String s){
-        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
